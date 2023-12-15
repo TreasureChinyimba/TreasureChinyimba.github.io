43cb4f05fbbeec19e1433f867dde07a6768a9d3c
Treasure Chinyimba <Treasurearthur4@gmail.com>
2023-12-16 00:17:30 +0200

    more updates

--- projects/file sharing/app.js
+++ projects/file sharing/app.js
@@ -3,11 +3,11 @@
   2. Scroll top
   3. Play / pause / seek
   4. CD rotate
-  5. Next / prve
+  5. Next / previous
   6. Random
   7. Next / repeat when ended
   8. Active song
-  9. Scroll acctive song into view
+  9. Scroll active song into view
   10. Play song when click
 */
 
